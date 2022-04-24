1ecc8d7b922262eccdd2bb3b31e966e8b9afb016
NoTimeForHero <notimeforhero@yandex.ru>
2022-04-24 03:31:38 +0300

    Polyfill GM_addStyle via GM_xmlHTTPRequest for non-standart clients

--- Steam Web Extensions.user.js
+++ Steam Web Extensions.user.js
@@ -1,20 +1,34 @@
 // ==UserScript==
 // @name         Steam Web Extensions
 // @namespace    http://github.com/NoTimeForHero/steam-web-extensions/
-// @version      0.0.2
+// @version      0.0.3
 // @description  Some useful extensions for Steam
 // @author       You
 // @match        https://steamcommunity.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=steamcommunity.com
-// @require      https://raw.githubusercontent.com/NoTimeForHero/steam-web-extensions/master/dist/index.js
-// @resource     IMPORTED_CSS https://raw.githubusercontent.com/NoTimeForHero/steam-web-extensions/master/dist/index.css
+// @require      https://raw.githubusercontent.com/NoTimeForHero/steam-web-extensions/master/dist/index.js?cache=ZnVuY3Rpb24gb
+// @resource     IMPORTED_CSS https://raw.githubusercontent.com/NoTimeForHero/steam-web-extensions/master/dist/index.css?cache=ZnVuY3Rpb24gb
 // @grant      GM_getResourceText
 // @grant      GM_addStyle
+// @grant      GM_xmlHTTPRequest
 // ==/UserScript==
 
+const gmFetch = (url, method = 'GET') => new Promise((onload, onerror) => GM_xmlhttpRequest ({ url, method, onload, onerror}));
+const loadCSS = async(resourceName, fallbackURL) => {
+    if (typeof GM_getResourceText === 'function' && typeof GM_addStyle === 'function') {
+        console.log('[UserScript->SWE] CSS inserted by GM_addStyle');
+        const cssImported = GM_getResourceText(resourceName);
+        GM_addStyle(cssImported);
+    } else {
+        console.log('[UserScript->SWE] CSS inserted by GM_xmlHTTPRequest');
+        const response = await gmFetch(fallbackURL);
+        const style = document.createElement('style');
+        style.textContent = response.response;
+        document.head.append(style);
+    }
+};
+
 (function() {
-    'use strict';
-    const my_css = GM_getResourceText("IMPORTED_CSS");
-    GM_addStyle(my_css);
+    loadCSS('IMPORTED_CSS', 'https://raw.githubusercontent.com/NoTimeForHero/steam-web-extensions/master/dist/index.css');
     // Your code here...
 })();
